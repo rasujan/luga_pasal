79661bb3e22eb2cbd7f916a6a0870dfe28f1cb22
sujan <sujan.rays99@gmail.com>
2020-01-30 09:23:58 +0545

    using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,32 @@
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Luga Pasal',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        accentColor: Colors.blueAccent,
-        fontFamily: 'Lato',
+
+
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+
+        title: 'Luga Pasal',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          accentColor: Colors.blueAccent,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      } ,
     );
   }
 }
-
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
 //  final String tille;
@@ -9,10 +12,12 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findbyId(productId);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('tille'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
